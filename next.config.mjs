f12b5fd666b51da465ce361dc17687988b77bb4a
kiflom <kiflomberihu2024@gmail.com>
2025-03-18 03:11:16 -0700

    added new get started page

--- next.config.mjs
+++ next.config.mjs
@@ -6,20 +6,20 @@ const withNextra = nextra({
 
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/quickstart',
+        permanent: true,
+      },
+    ]
+  },
   // ... Other Next.js config options
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/installation',
-        permanent: true,
-      },
-    ]
   },
 })
