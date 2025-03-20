639e6ffc1abceb07afe3456f2b37c173ed775233
kiflom <kiflomberihu2024@gmail.com>
2025-03-19 20:58:05 -0700

    added docs for stk push

--- src/app/_meta.js
+++ src/app/_meta.js
@@ -4,6 +4,7 @@ export default {
     payout: "Payout initiation",
     registerurl: "URL Registration",
     stkpush: "STK Push",
+    errorhandler: "Error Handling",
     apireference: "API Reference",
     FAQ: "Frequently Asked Questions",
 
